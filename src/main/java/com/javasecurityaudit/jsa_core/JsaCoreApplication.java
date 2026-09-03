5027d5ce5ebf3b38a4eaba320e96bafa5727a046
Lê Quốc Huy <lequochuy12012k4@gmail.com>
2026-09-03 20:23:16 +0700

    Fix timezone for PostgreSQL by adding TimeZone in Application

--- src/main/java/com/javasecurityaudit/jsa_core/JsaCoreApplication.java
+++ src/main/java/com/javasecurityaudit/jsa_core/JsaCoreApplication.java
@@ -1,11 +1,14 @@
 package com.javasecurityaudit.jsa_core;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JsaCoreApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(JsaCoreApplication.class, args);
 	}
 }
